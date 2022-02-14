ed04f1f78476bd4f1085f1738be466793136757d
upendra <upendrarajput9911@gmail.com>
2022-02-14 16:12:56 +0530

    done

--- block-BJaajc/index.test.js
+++ block-BJaajc/index.test.js
@@ -1,23 +1,21 @@
 const {
-    getFullName,
-    isPalindome,
-    getCircumfrence,
-    getArea
-} = require(`./index`);
+  getFullName,
+  isPalindome,
+  getCircumfrence,
+  getArea,
+} = require(`./code/index`)
 
 test('test for full name', () => {
-    expect(getFullName('alt', 'campus')).toBe('alt campus');
-    expect(getFullName('alt', 'campus')).not.toBe('altcampus');
-    expect(getFullName('bharat', 'babu')).not.toBe('bharatbabu');
-  });
-  
+  expect(getFullName('alt', 'campus')).toBe('alt campus')
+  expect(getFullName('alt', 'campus')).not.toBe('altcampus')
+  expect(getFullName('bharat', 'babu')).not.toBe('bharatbabu')
+})
 
-  
-  test('test for palindome', () => {
-    expect(isPalindome('wow')).toBe(true);
-    expect(isPalindome('tat')).toBe(true);
-    expect(isPalindome('gag')).toBe(true);
-    expect(isPalindome('campus')).not.toBe(true);
-    expect(isPalindome('babu')).not.toBe(true);
-    expect(isPalindome('bharat')).not.toBe(true);
-  });
+test('test for palindome', () => {
+  expect(isPalindome('wow')).toBe(true)
+  expect(isPalindome('tat')).toBe(true)
+  expect(isPalindome('gag')).toBe(true)
+  expect(isPalindome('campus')).not.toBe(true)
+  expect(isPalindome('babu')).not.toBe(true)
+  expect(isPalindome('bharat')).not.toBe(true)
+})
